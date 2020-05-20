d622fd35d45362551de234ae3b9c51fbcdf31561
rachelemmer <rachelaemmer@gmail.com>
2020-05-20 15:18:05 -0600

    add hike table

---
+++ migrations/20200520144234_hike.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("hike", hike => {
+        hike.increments()
+        hike.integer("user_id").references("id").inTable("user")
+        hike.integer("mountain_id").references("id").inTable("mountain")
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("hike")
+};
